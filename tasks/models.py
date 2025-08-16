f5fa5eeea2d9234ac65a90f6a3e2c842bd5ed143
GuilhermeRanquine <guilhermemacedoranquine2003@gmail.com>
2025-08-16 00:23:09 -0300

    Aqui eu dei um makemigrations no terminal após fazer alterações em models.py e settings.py
    Com esse comando no terminal criei um arquivo de migração, fiz a migração e criei uma tabela no banco de dados.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Documentação:
+# Definimos o nosso modelo de Tarefa.
+# O models.Model diz ao Django que esta classe é um modelo.
+class Task(models.Model):
+    # Relacionamento com o usuário.
+    # Quando um usuário é deletado, todas as suas tarefas também são.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # O texto da tarefa.
+    # CharField é usado para textos curtos. max_length é obrigatório.
+    title = models.CharField(max_length=200)
+
+    # Status da tarefa.
+    # O valor padrão (default=False) significa que a tarefa não está concluída ao ser criada.
+    completed = models.BooleanField(default=False)
+
+    # Documentação para o retorno em string (opcional, mas boa prática).
+    # Se você imprimir um objeto Task, ele retornará o título da tarefa.
+    def __str__(self):
+        return self.title
