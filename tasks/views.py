08601cfb0f407b844541042fe4e81b175d4479ae
GuilhermeRanquine <guilhermemacedoranquine2003@gmail.com>
2025-08-16 00:33:27 -0300

    feat: Adiciona views e URLs para listar tarefas

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Task
 
-# Create your views here.
+@login_required
+    def task_list(request):
+        """
+    Exibe a lista de tarefas do usuário logado.
+    """
+     # Filtra as tarefas para mostrar apenas as do usuário atual
+     tasks = Task.objects.filter(user=request.user).order_by('completed', '-id')
+     
+    # Passa as tarefas para o template (a página HTML)
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tasks/task_list.html', context)
